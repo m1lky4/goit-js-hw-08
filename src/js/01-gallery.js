fc73e26a8f9e1957fd6e093b3704cbbeea32f046
m1lky4 <temanafanin1337@gmail.com>
2023-04-28 17:48:21 +0200

    check

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+
+
+
+
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = galleryItems.map((elem) => {
+
+  return `<li class="gallery__item"><a class="gallery__link" href="${elem.original}"><img class="gallery__image" src="${elem.preview}" alt="${elem.description}"/></a></li>`;
+}).join('');
+
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt',
+  captionDelay: 250});
