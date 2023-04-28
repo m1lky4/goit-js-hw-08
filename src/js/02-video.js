405f4296c7b1a8ba9cab77ada0d5c3c657fda9b0
m1lky4 <temanafanin1337@gmail.com>
2023-04-28 20:26:01 +0200

    partly done the job

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+player.on('timeupdate', throttle((timeupdate) => {
+        localStorage.setItem('videoplayerCurrentTime', JSON.stringify(timeupdate));;
+}, 1000));
+    
+let savedTime = localStorage.getItem('videoplayerCurrentTime');
+let parsedTime = JSON.parse(savedTime);
+player.setCurrentTime(parsedTime.seconds);
+
+   
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
 
+
+const form = document.querySelector('.feedback-form');
+
+
+form.addEventListener('input', onFormInput);
+function onFormInput(e) {
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+    const formResult = {
+        email,
+        message
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(formResult));
+    
+};
+
+const savedFormStorage = localStorage.getItem('feedback-form-state');
+let parsedFormStorage = JSON.parse(savedFormStorage);
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    form.reset();   
+    console.log(parsedFormStorage);
+}
